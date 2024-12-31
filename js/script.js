72ea1c5ee024ffe35cdd23d0239e745d3d2af779
soni <sonisah273b@gmail.com>
2024-12-31 16:02:07 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// swipper carousel
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,  // Delay between slide transitions in milliseconds (2.5 seconds)
+      disableOnInteraction: false, // Keeps autoplay going even if the user interacts with the slider
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+ 
